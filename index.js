5358bf84a74f70495a04e3ac0dc1a060cc4079c5
jhunethan01 <jhunethan23@gmail.com>
2023-12-28 14:18:58 +0000

    enable cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors()); 
+
 require('dotenv').config();
 
 const mysql = require('mysql2');
