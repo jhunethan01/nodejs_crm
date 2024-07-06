a8f44022418857e91c5db00abfd9da24e400b5d9
Ethan Lay <jhunethan23@gmail.com>
2024-07-06 16:15:49 +0100

    {"message":"connection is insecure (try using `sslmode=require`)"}

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const pool = new Pool({
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT
+    port: process.env.POSTGRES_PORT,
+    ssl: {
+        regectUnauthorized: false
+    }
 });
 
 app.get('/', (req, res) => {
